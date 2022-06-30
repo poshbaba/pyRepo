d8387fdee2b93de1f46e7284f059c5ed3d9de93b
poshbaba <omotola.aborisade@yahoo.com>
2022-06-30 14:33:54 -0500

    Updated freebies for 30+ people

--- RollerCoaster.py
+++ RollerCoaster.py
@@ -13,15 +13,17 @@ if height >= 120:
     elif age <= 18:
         Bill = 15
         print("Youth tickects are $15")
+    elif age >= 30 and age <= 40:
+        print("You get a free ride on US!")
     else:
         print("Adults tickets are $20")
         Bill = 20
     Response = input("Do you want a photo? Type Y for Yes and N for No  ")
     if Response == "Y":
         Bill += 3
-        print(f"Your final bill is {Bill}")
+        print(f"Your final bill is ${Bill}")
     else:
-        print(f"Your final bill is {Bill}")
+        print(f"Your final bill is ${Bill}")
 
 else:
     print("You are too short to ride the rollercoaster")
