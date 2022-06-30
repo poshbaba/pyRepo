5de4a9c2ec25e15a0c22dfcadaf1e7b73121c046
poshbaba <omotola.aborisade@yahoo.com>
2022-06-30 14:13:47 -0500

    updated comments to Pizza

--- Pizza.py
+++ Pizza.py
@@ -1,5 +1,6 @@
-# 🚨 Don't change the code below 👇
-print("Welcome to Python Pizza Deliveries!")
+#  Prints Bill based on Pizza size, Pepperoni and Cheese
+#Uses Nested if and elif statements
+print("Welcome to PoshBaba Pizza!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
