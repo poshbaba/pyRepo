5538206c75276cc699ed5f02d5246552096a6163
poshbaba <omotola.aborisade@yahoo.com>
2022-06-30 14:09:38 -0500

    Pizza

---
+++ Pizza.py
@@ -0,0 +1,35 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+bill = 0
+#Write your code below this line 👇
+if size == "S":
+  bill = 15
+  print(f"Small size pizza is ${bill}")
+  if add_pepperoni == "Y":
+    bill += 2
+    print(f"Bill for Small Pizza and Pepperoni is ${bill}")
+  
+elif size == "M":
+  bill = 20
+  print(f"Medium size pizza is ${bill}")
+  if add_pepperoni == "Y":
+    bill += 3
+    print(f"Bill for Medium Pizza is ${bill}")    
+else:
+  bill = 25
+  print(f"Big size pizza is ${bill}")
+  if add_pepperoni == "Y":
+    bill += 3
+    print(f"Bill for Large Pizza is ${bill}")
+
+if extra_cheese == "Y":
+  bill += 1
+  print(f"Your total bill is ${bill}")
+
+
+
+
